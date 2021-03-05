cf05d930940acb7d7a4c9f4e2a9835d3e06d190e
ayah <breakayah92@gmail.com>
2021-03-06 01:26:36 +0200

    solution1

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ Write a function called sum() that takes in two numbers as arguments and then re
 "The sum of 4 and 7 is 11."
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
-
+//eslint-disable-line
 // Write your code here
-function sum(a, b) { //eslint-disable-line
+function sum(a, b) { 
     let sum = a + b ; 
     let con1 =a;
     let con2 =b;
